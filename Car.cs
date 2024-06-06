5f2a3126c521c0b99872f153270fa31705d8bbf4
justin long <justinxlong93@gmail.com>
2024-06-06 11:13:10 -0500

    Classes Practice

---
+++ Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morning_Practice
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,66 +7,23 @@ namespace Morning_Practice
 
 
         static void Main(string[] args)
-        {
+        { 
+            
+
+            var truck = new Car();
+            truck.Make = "Ford";
+            truck.Year = 2017;
+            truck.Model = "F150";
 
 
-            //TODO:
-
-            // Create an int array and populate it with numbers 1-10
-            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-
-            /* Create a list of type int
-             * Name the list "evens"
-             */
-            var evens = new List<int>();
-
-            /* Create another list of type int
-             * Name the list "odds"
-             */
-            var odds = new List<int>();
-
-            /* Using either a foreach or for loop,
-            * iterate through the array you populated with 10 numbers.
-            * Inside the scope of the loop,
-            * check to see if each number in the array is even or odd.
-            * If the number is even, add it to the evens list.
-            * If the number is odd, add it to the odds list.
-            */
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    evens.Add(i);
-                }
-                else 
-                {
-                    odds.Add(i);
-                }    
-            }
+            Console.WriteLine($"The truck make is {truck.Make}");
+            Console.WriteLine($"The truck model is {truck.Model}");
+            Console.WriteLine($"The truck year is {truck.Year}");
 
 
-            /* Using a foreach loop,
-             * display the numbers in your "evens" list
-             */
-            Console.WriteLine("Evens:");
-            foreach (var item in evens)
-            {
-                
-                Console.WriteLine(item);
-            }
-
-            /* Using a for loop,
-             * display the numbers in your "odds" list
-             */
-            Console.WriteLine("Odds:");
-            foreach (var item in odds)
-            {
-                Console.WriteLine(item);
-            }
         }
 
-
+     
 
     }
 
